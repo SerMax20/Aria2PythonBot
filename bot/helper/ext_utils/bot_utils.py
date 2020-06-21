1a62f8b01e88a0741c8f27b3bc864080dfafd505
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-06-21 07:21:53 +0000

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -13,12 +13,12 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading"
-    STATUS_DOWNLOADING = "Downloading"
-    STATUS_WAITING = "Queued"
+    STATUS_UPLOADING = "📤Uploading"
+    STATUS_DOWNLOADING = "📥Downloading"
+    STATUS_WAITING = "⌛Queued"
     STATUS_FAILED = "Failed. Cleaning download"
-    STATUS_CANCELLED = "Cancelled"
-    STATUS_ARCHIVING = "Archiving"
+    STATUS_CANCELLED = "✘Cancelled"
+    STATUS_ARCHIVING = "✉Archiving"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
